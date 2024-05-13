152865bc33ea43c3546fe8ad073da0175d07d30d
Eun Oh <bdohhhhh@gmail.com>
2024-05-13 20:31:25 +0900

    240513 commit test??

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,10 +12,6 @@ type TodoProps = {
 const Card = memo(({ todo, toDos, inputted, setToDos }: TodoProps) => {
     // 투두 완료로 처리
     const completeToDo = (copied: ToDo[], cases: string) => {
-        // if (todo.isDone) {
-        //     alert("이미 완료되었습니다!");
-        //     return;
-        // }
         // 현재 컴포넌트 데이터(todo)의 id 와 일치하는 id를 가진 객체를 toDos 배열에서 찾아서
         // 찾은 객체의 isDone 상태를 true 로 변경
         const mapped = copied.map((e) => {
